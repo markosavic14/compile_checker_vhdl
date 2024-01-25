f4b14b96f8370517b7c6a9be9efd4b96f965c6d8
markosavic14 <markosavic14@gmail.com>
2024-01-25 17:19:06 +0100

    Replaced os.system call with subprocess module call

--- src/compile_checker_vhdl.py
+++ src/compile_checker_vhdl.py
@@ -1,4 +1,4 @@
-# version: 0.2
+# version: 0.2c
 # autor Marko Savic (markosavic14@gmail.com)
 # Januar 2023.
 # za potrebe LPRS1 nastavne grupe
@@ -6,15 +6,16 @@
 # zahteva python i vcom+vlib (dolaze uz instalaciju quartusa/queste)
 # pokrenuti sa admin pravima (preimenuje datoteke, moze da baci gresku ako nema prava)
 # skripta pisana i testirana na Windowsu
-# po potrebi izmeniti path_to_tool i filename stringove
+# po potrebi izmeniti path_to_tool string
 
 import os
+import subprocess
 from pathlib import Path
 from shutil import rmtree, copy, copytree
 import transliterate
 
-# putanja do vcom i vlib alata
-# (Quartus -> C:/intelFPGA_lite/xx.x/modelsim_ase/win32aloem/)
+# putanja do alata
+# Quartus -> C:/intelFPGA_lite/xx.x/modelsim_ase/win32aloem/
 # Questa -> (C:/questasim64_xx.xx/win64/)
 path_to_tool = 'C:/questasim64_2022.4/win64/'
 
@@ -29,7 +30,8 @@ def rchop(s, suffix):
 def strip_end(text, split_char):
     for i, char in enumerate(reversed(text)):
         if char == split_char:
-            return text[:len(text)- (i+1)]
+            return text[:len(text) - (i + 1)]
+
 
 def rm_folder_and_conts(name):
     if os.path.exists(name):
@@ -45,16 +47,17 @@ def m_dfs(s, tree, filepath=""):
     filepath += s
     os.chdir(s)
     subfiles = [name for name in os.listdir('.')]
-    transliterate.t_and_rename(subfiles)
+    transliterate.t_and_rename(subfiles)            # transliteracija cirilice i slova sa dijakriticima
     ret_str = []
-    for file in subfiles:
-        if os.path.isdir(f"{file}"):
-            m_dfs(f'{file}/', tree, filepath)
-        elif 1 <= len(subfiles) <= 3:
-            if file.endswith('.vhd'):
-                ret_str.append(filepath + file)
+    for s_file in subfiles:
+        if os.path.isdir(f"{s_file}"):
+            m_dfs(f'{s_file}/', tree, filepath)
+        elif 1 <= len(subfiles) <= 4:
+            if s_file.endswith('.vhd'):
+                ret_str.append(filepath + s_file)
         else:
-            print("Nepravilan broj datoteka unutar foldera.")
+            print(s_file)
+            print("Nepravilna struktura foldera.")
     if len(ret_str) != 0:
         tree.append(ret_str)
     os.chdir("../")
@@ -65,7 +68,7 @@ def make_subfolders(dst):
         for i, char in enumerate(dst):
             if char == '/':
                 temp = dst[:i]
-                dst = dst[i+1:]
+                dst = dst[i + 1:]
                 if not os.path.exists(temp):
                     os.makedirs(temp)
                 os.chdir(temp)
@@ -74,21 +77,24 @@ def make_subfolders(dst):
                 os.chdir('../')
                 break
 
-#dodati output u string
+
+# dodati output u string
 
 def copy_tree(src, dst):
-        for file in src:
-            text = file
-            for i, char in enumerate(reversed(text)):
-                if char == '/':
-                    temp_filename = text[len(text) - i:]
-                    temp_src = text[:len(text) - i]
-                    make_subfolders('output/' + dst + temp_src[5:])
-                    copy(file, 'output/' + dst + file[5:])
-                    break
+    for s_file in src:
+        text = s_file
+        for i, char in enumerate(reversed(text)):
+            if char == '/':
+                temp_src = text[:len(text) - i]
+                make_subfolders('output/' + dst + temp_src[5:])
+                copy(s_file, 'output/' + dst + s_file[5:])
+                break
+
 
 if __name__ == "__main__":
-    os.chdir("../")
+    #os.chdir("../")        # po potrebi zakomentarisati ukoliko se skripta pokrece rucno a ne preko run.bat skripte
+
+    # Provera da li postoji input folder i da li je popunjen
     if not os.path.exists('input/'):
         print("Ne postoji input folder")
         exit(1)
@@ -103,8 +109,8 @@ if __name__ == "__main__":
     file_tree = []
     m_dfs('input/', tree=file_tree)
 
-    #napravi radni folder za alat, ako i output folder
-    os.system(f"{path_to_tool}vlib.exe work")
+    # napravi radni folder za alat, ako i output folder
+    subprocess.run(f"{path_to_tool}vlib.exe work")
     os.makedirs('output/')
 
     for file in file_tree:
@@ -112,10 +118,17 @@ if __name__ == "__main__":
         for elem in file:
             instr += f' "{elem}"'
 
-        if os.system(instr):
+        ret = subprocess.run(instr, capture_output=True);
+        if ret.returncode:
             copy_tree(file, 'comp_fail')
+            print(file[0] + '\n' + file[1])
+            print("COMP FAIL")
         else:
             copy_tree(file, 'comp_pass')
+            print(file[0] + '\n' + file[1])
+            print("COMP PASS")
+
+        print('\n############################\n')
 
     # po zavrsetku skripte izbrisi radni folder vcom alata
     rm_folder_and_conts('work/')
